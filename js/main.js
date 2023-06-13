4a0f6bd45163ced76b21184ce27da86f500dcc77
Ayush Kumar <ayushkumar982191@gmail.com>
2023-06-13 21:26:44 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let nav=document.querySelector(".navbar");
+window.onscroll=function(){
+    if(document.documentElement.scrollTop>20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+let navbar=document.querySelector(".nav-link");
+let navcoll=document.querySelector(".navbar-collapse.collapse");
+navbar.forEach(function(a){
+    a.addEventListner("click",function(){
+        navcoll.classList.remove("show")
+    })
+})
